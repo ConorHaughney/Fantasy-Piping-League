6ac05bc877cf32acd6ae0c03da03fd1b6cc4f1c4
Conor Haughney <cphaughney99@gmail.com>
2025-07-24 17:27:19 +0100

    Staring Structure

---
+++ src/main/java/com/example/FantasyPipingLeague/FantasyPipingLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.example.FantasyPipingLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyPipingLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyPipingLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FantasyPipingLeague/FantasyPipingLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FantasyPipingLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyPipingLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
